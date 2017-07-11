3f622732251440b6ebdca84f1708ac053ebf7931
WisherBG <wisher@abv.bg>
2017-07-11 16:40:55 +0300

    Better .gitignore

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
- class Post(models.Model):
+class Post(models.Model):
   author = models.ForeignKey('auth.User')
   title = models.CharField(max_length=200)
   text = models.TextField()
