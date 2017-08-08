55445c1e36aeb71e4d76195cbc7943a0b4d0ac0c
WisherBG <wisher@abv.bg>
2017-08-08 14:52:48 +0300

    Added detailed blog post view and 404 handling

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.post_list, name='post_list'),
-  url(r'detail/?P<pk>(\d+)/', views.post_detail, name='post_detail')
+  url(r'^detail/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
 ]
