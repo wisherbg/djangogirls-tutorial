152b1eae360cd7bb474f21dc946a5c5788d18ce5
WisherBG <wisher@abv.bg>
2017-08-09 16:51:47 +0300

    Added draft page which shows posts that are not yet published

--- blog/views.py
+++ blog/views.py
@@ -21,12 +21,13 @@ def add_new(request):
     if form.is_valid():
       post = form.save(commit=False)
       post.author = request.user
-      post.published_date = timezone.now()
       post.save()
       return redirect('post_detail', pk=post.pk)
   else:
     form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
 def post_edit(request, pk):
   post = get_object_or_404(Post, pk=pk)
   if request.method == 'POST':
@@ -34,11 +35,14 @@ def post_edit(request, pk):
     if form.is_valid():
       post = form.save(commit=False)
       post.author = request.user
-      post.published_date = timezone.now()
       post.save()
       return redirect('post_detail', pk=post.pk)
   else:
     form = PostForm(instance=post)
   return render(request, 'blog/post_edit.html', {'form': form})
+
+def draft_posts(request):
+  posts = Post.objects.filter(published_date__isnull=True)
+  return render(request, 'blog/draft_posts.html', {'posts': posts})
 
 
