cf305473d0d75776e0fd958b6b7bd1690ccd152d
WisherBG <wisher@abv.bg>
2017-08-09 15:59:39 +0300

    Show Edit and New only to authenticated users

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.post_list, name='post_list'),
-  url(r'^detail/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+  url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
   url(r'^post/new/$', views.add_new, name='add_new'),
   url(r'post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit')
 ]
